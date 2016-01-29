5e7b3baef9db4241ec59eaeee8b7443cdc2549fe
kassava <asshironin@gmail.com>
2016-01-29 16:47:30 +0300

    Initial commit

---
+++ app/src/main/java/ru/android/develop/shiz/arduionocommander/LoginDataChecker.java
@@ -0,0 +1,14 @@
+package ru.android.develop.shiz.arduionocommander;
+
+/**
+ * Created by ultra on 29.01.2016.
+ */
+public class LoginDataChecker {
+    public static String[] getCredentials() {
+        String[] credentials = new String[] {
+                "loging:pass"
+        };
+
+        return credentials;
+    }
+}
---
+++ app/src/main/java/ru/android/develop/shiz/arduionocommander/MainActvity.java
@@ -0,0 +1,22 @@
+package ru.android.develop.shiz.arduionocommander;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by ultra on 29.01.2016.
+ */
+public class MainActvity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
